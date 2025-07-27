84febfd3370069d66e3b8c71bfbae7238e8b8821
Himanshu Jindal <164390289+Vasu-04@users.noreply.github.com>
2025-07-27 21:25:58 +0530

    Todo List Basic Project - Creation, Updation, Deletion

--- REACT.JS/Learning_React/src/TodoProject.jsx
+++ REACT.JS/Learning_React/src/TodoProject.jsx
@@ -1,19 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./TodoProject.css"
+
+
+
+
 const TodoProject = () => {
-  return (
-    <div className='mainDiv'>
-        <h1>Your WishList</h1>
-        <div className='inputBar'>
-            <input placeholder='Enter Wishlist...'></input>
-            <button>Add</button>
+
+    const [wishListValue, setWishListValue] = useState("");
+    const wishlistInputChange = (event) => {
+        setWishListValue(event.target.value)
+    }
+
+    const [wishlistList, setwishlistList] = useState([]);
+    const addBtnClick = () => {
+        const id = wishlistList.length == 0 ? 1 : wishlistList[wishlistList.length - 1].id + 1
+        const newWish = {
+            id: id,
+            activity: wishListValue,
+            status: false
+        }
+        setWishListValue("")
+        setwishlistList([...wishlistList, newWish])
+    }
+
+    // const checkBoxChange = (id) => {             // direct mutation
+    //     console.log(id)
+    //     setwishlistList(
+    //         wishlistList.filter((item) => {
+    //             if (item.id === id) {
+    //                 item.status = true;
+    //             }
+    //         })
+    //     )
+    // }
+
+    const checkBoxChange = (id) => {
+        setwishlistList(
+            wishlistList.map((item) => {
+                if (item.id === id) {
+                    return { ...item, status: !item.status };
+                }
+                return item;
+            })
+        );
+    }
+
+    const deleteWish = (id) => {
+        setwishlistList(
+            wishlistList.filter((item) => {
+                if (item.id !== id) {
+                    return item;
+                }
+            })
+        )
+    }
+
+    console.log(wishlistList)
+    return (
+        <div className='mainDiv'>
+            <h1>Your WishList</h1>
+            <div className='inputBar'>
+                <input value={wishListValue} placeholder='Enter Wishlist...' onChange={wishlistInputChange}></input>
+                <button onClick={addBtnClick}>Add</button>
+            </div>
+            <div className='wishlists'>
+                {
+                    (wishlistList.size != 0) ?
+                        <ul>
+                            {
+                                wishlistList.map((wishList) => {
+                                    return <li key={wishList.id}>
+                                        <div className='wishListItem'>
+                                            <div>
+                                                <input type='checkbox' value={wishList.status} onChange={() => checkBoxChange(wishList.id)}></input>
+                                                <p>{wishList.activity}</p>
+                                            </div>
+                                            <i className="fas fa-trash" onClick={() => deleteWish(wishList.id)}></i>
+                                        </div>
+                                    </li>
+                                })
+                            }
+                        </ul>
+                        : <div></div>
+                }
+            </div>
+            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
         </div>
-        <div className='wishlists'>
-            
-        </div>
-        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-    </div>
-  )
+    )
 }
 
 export default TodoProject
